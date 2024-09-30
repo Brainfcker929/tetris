3c175120c9ee15d7a7419f1cbac259b846d53278
Paul Rusch <pauly929@hotmail.de>
2024-09-30 19:48:52 +0200

    adding tetrominos

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const COLS = 10;
 const ROWS = 20;
 const BLOCK_SIZE = Math.min(canvasEl.width / COLS, canvasEl.height / ROWS);
 
+const TETROMINOS = {
+  I: [[1, 1, 1, 1]],
+  O: [[1, 1], [1, 1]],
+  T: [[0, 1, 0], [1, 1, 1]],
+  S: [[0, 1, 1], [1, 1, 0]],
+  Z: [[1, 1, 0], [0, 1, 1]],
+  J: [[1, 0, 0], [1, 1, 1]],
+  L: [[0, 0, 1], [1, 1, 1]]
+};
+
+
 function drawGrid() {
     canvasContext.strokeStyle = 'gray';
     for (let x = 0; x < COLS; x++) {
